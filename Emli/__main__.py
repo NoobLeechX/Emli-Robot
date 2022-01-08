7b5fa1cfe35864391d3bf31c7c3f183f50b6db1e
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2022-01-08 15:17:42 +0530

    Update __main__.py

--- Emli/__main__.py
+++ Emli/__main__.py
@@ -128,7 +128,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("EmikoRobot.modules." + module_name)
+    imported_module = importlib.import_module("Emli.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
