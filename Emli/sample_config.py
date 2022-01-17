2ae176d2a3679d0f017b74defc938ecf5458e5db
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2022-01-17 14:43:03 +0530

    Update sample_config.py

--- Emli/sample_config.py
+++ Emli/sample_config.py
@@ -65,6 +65,7 @@ class Config(object):
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    VIRUS_API_KEY = "awoo" # For scan file , get one from  https://www.virustotal.com/gui/join-us
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
