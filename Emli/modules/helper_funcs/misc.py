8f69b6189dfbfa9999cd00c05c8e56625d95cf24
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2022-01-24 08:23:50 +0530

    Update misc.py

--- Emli/modules/helper_funcs/misc.py
+++ Emli/modules/helper_funcs/misc.py
@@ -131,3 +131,12 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
 def is_module_loaded(name):
     return name not in NO_LOAD
+
+
+
+def delete(delmsg, timer):
+    sleep(timer)
+    try:
+        delmsg.delete()
+    except:
+        return
