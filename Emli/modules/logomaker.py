54b35994436d5e4f9e25feea54c2bfedb1c7e231
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2022-03-03 09:21:47 +0530

    Update logomaker.py

--- Emli/modules/logomaker.py
+++ Emli/modules/logomaker.py
@@ -15,7 +15,7 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./Cutiepii_Robot/utils/Logo/blackbg.jpg')
+    img = Image.open('./Emli/utils/Logo/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
