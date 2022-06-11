5faad78443fb6b9e37204d83a838f2f4dde9a3e1
NoobLeechX <96004031+NoobLeechX@users.noreply.github.com>
2022-06-11 13:22:32 +0530

    Update __init__.py

--- Emli/modules/sql/__init__.py
+++ Emli/modules/sql/__init__.py
@@ -1,14 +1,15 @@
-from Emli import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from Emli import DB_URL
 
 
 def start() -> scoped_session:
     engine = create_engine(DB_URL, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+    return scoped_session(sessionmaker(bind=engine, autoflush=True))
 
 
 BASE = declarative_base()
